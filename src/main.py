b3a2677689a1372bbb0f272fb51bf78f421d8865
joslack <70915892+joslack@users.noreply.github.com>
2023-04-20 22:55:03 -0400

    tested simple text input and response

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+
+load_dotenv()
+
+def main():
+    # TODO: load and vectorize pdf content
+
+    # TODO: output a list of the papers
+
+    # infinite loop
+    # TODO: use user input to find a specific paper, and find the content most likely to be associated with their query
+
+    # TODO: generate a prompt with langchain
+
+    # TODO: use gpt turbo to ingest and answer the question
+    user_input = input("Enter a question about AI: ")
+    template = f"You are an expert in the field of Artificial Intelligence, answer the following question: {user_input}"
+    
+    openaichat = OpenAI(temperature=0.5)
+    response = openaichat(template)
+    print(response)
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
